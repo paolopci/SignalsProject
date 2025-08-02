2d7a487848eb148267074d2118c5820b9fe7e817
Paolo Paci <paolopci@yahoo.it>
2025-08-02 11:13:53 +0200

    rilevamento delle modifiche con RxJs

--- src/app/app1.component/app1.component.ts
+++ src/app/app1.component/app1.component.ts
@@ -1,39 +1,21 @@
-import { Component, inject, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { interval } from 'rxjs';
 
 
 @Component({
   selector: 'app1-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app1.component.html',
   styleUrl: './app1.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush // Optional, for performance optimization
 })
 export class App1Component {
-  title = 'Paolo Paci';
-  readonly changeDetector = inject(ChangeDetectorRef);
-  counter = 0;
 
-  doNothing() { }
-
-
-  calculateValue() {
-    console.log('calculateValue called');
-    return 42;
-  }
-
+  counter$ = interval(1000); // Emits a value every second
 
   constructor() {
-    setInterval(() => {
-      this.counter++;
-      console.log('Counter:', this.counter);
-    }, 1000);
-
-    // setInterval(() => {
-    //   this.changeDetector.detectChanges();
-    // }, 5000);
-
-
   }
 
 }
