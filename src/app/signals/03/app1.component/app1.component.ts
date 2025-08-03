7ea5b53f6909111f9a1cd009fbcb64c6edbf45e4
Paolo Paci <paolopci@yahoo.it>
2025-08-03 11:06:29 +0200

    installato bootstrap 5  e implementato .set e update per signals

--- src/app/signals/03/app1.component/app1.component.ts
+++ src/app/signals/03/app1.component/app1.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { mysignal } from './my-signal';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app01-component',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './app1.component.html',
   styleUrl: './app1.component.scss',
@@ -11,12 +12,12 @@ import { mysignal } from './my-signal';
 })
 export class App01Component {
 
-  // readonly firstSignal = signal(42);
-  // readonly secondSignal = signal('Hello, Signals!');
+  readonly firstSignal = signal(42);
+  readonly secondSignal = signal('Hello, Signals!');
 
 
-  readonly firstSignal = mysignal(42);
-  readonly secondSignal = mysignal('Hello, Signals!');
+  // readonly firstSignal = mysignal(42);
+  // readonly secondSignal = mysignal('Hello, Signals!');
 
   constructor() {
     // You can use signals in the constructor or methods
@@ -25,6 +26,17 @@ export class App01Component {
     console.log('Second Signal:', this.secondSignal());
 
   }
+
+
+  setSignal() {
+    this.firstSignal.set(100);
+  }
+
+  updateSignal() {
+    this.firstSignal.update(value => value + 1);
+    console.log('First Signal:', this.firstSignal());
+  }
+
 
 
 
