8da28897a674d9e49565bcea9023b3e3605b74a0
Paolo Paci <paolopci@yahoo.it>
2025-08-03 14:38:39 +0200

    esempio computed con data e ora

--- src/app/signals/03/app02.component/app02.component.ts
+++ src/app/signals/03/app02.component/app02.component.ts
@@ -13,7 +13,25 @@ export class App02Component {
   readonly firstSignal = signal(42);
   readonly derived = computed(() => this.firstSignal() * 2);
 
+  data = new Date().toLocaleDateString();// la data di oggi si calcola una sola volta non serve signal
+  dataOra = signal(new Date().toLocaleTimeString())
+
+  readonly dataOraString = computed(() => {
+    return `data: ${this.data} ora: ${this.dataOra()}`;
+  });
+
+
+
   val = this.derived();
+
+  constructor() {
+    // Aggiorna la data e ora ogni secondo
+    setInterval(() => {
+      this.dataOra.set(new Date().toLocaleTimeString());
+    }, 1000);
+  }
+
+
 
   increment() {
     this.firstSignal.update(value => value + 1);
